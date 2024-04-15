5ee9caef5d8024e15552c950a9003506c06d0c52
Matt Wicks <matt.wicks@gmail.com>
2024-04-16 07:11:39 +1000

    removed ordering api as not demoing it

--- src/eShop.AppHost/Program.cs
+++ src/eShop.AppHost/Program.cs
@@ -16,7 +16,6 @@ var postgres = builder.AddPostgres(
     .WithPgAdmin();
 
 var catalogDb = postgres.AddDatabase("CatalogDB");
-var orderDb = postgres.AddDatabase("OrderingDB");
 
 // Identity Providers
 
@@ -27,9 +26,6 @@ var idp = builder.AddKeycloakContainer("idp", tag: "23.0", configPath: "../Keycl
 builder.AddProject<Catalog_Data_Manager>("catalog-db-mgr")
     .WithReference(catalogDb);
 
-builder.AddProject<Ordering_Data_Manager>("ordering-db-mgr")
-    .WithReference(orderDb);
-
 // API Apps
 
 var catalogApi = builder.AddProject<Catalog_API>("catalog-api")
@@ -39,9 +35,6 @@ var basketApi = builder.AddProject<Basket_API>("basket-api")
     .WithReference(basketStore)
     .WithReference(idp);
 
-var orderingApi = builder.AddProject<Ordering_API>("ordering-api")
-    .WithReference(orderDb)
-    .WithReference(idp);
 
 // Apps
 
@@ -49,13 +42,11 @@ var orderingApi = builder.AddProject<Ordering_API>("ordering-api")
 var webApp = builder.AddProject<WebApp>("webapp", launchProfileName: "https")
     .WithReference(catalogApi)
     .WithReference(basketApi)
-    .WithReference(orderingApi)
     .WithReference(idp);
 
 // Inject the project URLs for Keycloak realm configuration
 idp.WithEnvironment("WEBAPP_HTTP", () => webApp.GetEndpoint("http").Url);
 idp.WithEnvironment("WEBAPP_HTTPS", () => webApp.GetEndpoint("https").Url);
-idp.WithEnvironment("ORDERINGAPI_HTTP", () => orderingApi.GetEndpoint("http").Url);
 
 // Inject assigned URLs for Catalog API
 catalogApi.WithEnvironment("CatalogOptions__PicBaseAddress", () => catalogApi.GetEndpoint("http").Url);
